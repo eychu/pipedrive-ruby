e44e3a694e43fbac1f391cfbdc198a623014f361
Alexey Chuvashov <alexeychu@gmail.com>
2016-03-21 09:35:47 +0400

    add remote_link to file

--- lib/pipedrive/file.rb
+++ lib/pipedrive/file.rb
@@ -1,4 +1,11 @@
 module Pipedrive
   class File < Base
+
+    class << self
+      def remote_link(opts = {})
+        res = post "#{resource_path}/remoteLink", body: opts
+        res.success? ? res['data'] : bad_response(res, opts)
+      end
+    end
   end
 end
